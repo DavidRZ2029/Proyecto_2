989b1ffb22a4ed15c2e1c776097b720897c805b4
Toño <yantonioyl@gmail.com>
2026-06-21 13:40:47 -0600

    Add initial setup for graficador_disco.py

--- graficador_disco.py
+++ graficador_disco.py
@@ -1,1 +1,19 @@
+import os
+import pygame
 
+
+anchodelaventana = 1200          
+altodelaventana = 750           
+profundidadpermitidadanalizar = 6     
+
+BLANCO = (255, 255, 255)
+NEGRO = (20, 20, 20)
+GRIS = (240, 240, 240)
+GRIS_OSCURO = (44, 62, 80)
+VERDE = (39, 174, 96)
+GRIS_TEXTO = (90, 90, 90)
+
+PALETA = [
+    (78, 121, 167), (242, 142, 43), (225, 87, 89), (118, 183, 178), (89, 161, 79), (237, 201, 72),
+    (176, 122, 161), (255, 157, 167), (156, 117, 95), (186, 176, 172), (109, 204, 218), (255, 110, 110),
+]
